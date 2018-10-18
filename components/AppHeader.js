040c11ea9a67759f7a768f61a469e9ea67265e3d
ben-pao <paohaveyouben@gmail.com>
2018-10-18 03:39:10 -0700

    header proto

---
+++ components/AppHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'react-native-elements';
+
+const AppHeader = ({ headerText }) => { // { headerText } === prop.headerText
+  return (
+    <Header
+      centerComponent={{ text: headerText, style: {color: 'white'} }}
+      outerContainerStyles={{ backgroundColor: '#E62117'}}
+    />
+  )
+};
+
+export default AppHeader;
