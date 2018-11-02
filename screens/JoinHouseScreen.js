9ec347fbb1479ec1e64359ce1ffee72b3435887a
Mary Chern <mary.chern10@gmail.com>
2018-11-01 19:22:09 -0700

    addicted to ballin'

--- screens/JoinHouseScreen.js
+++ screens/JoinHouseScreen.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, createDrawerNavigator } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity,  KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
 import * as firebase from 'firebase';
 import { Keyboard } from 'react-native';
 
 class JoinHouse extends Component {
-  // static navigationOptions = {
-  //   drawerLabel: 'Home',
-  //   // drawerIcon: ({ tintColor }) => (
-  //   //   <Image
-  //   //     source={require('./chats-icon.png')}
-  //   //     style={[styles.icon, {tintColor: tintColor}]}
-  //   //   />
-  //   // ),
-  // };
+
+  state = {
+    houseID: ''
+  }
+
   render() {
     const { containerStyle,
+            wrapperStyle,
             headerStyle,
+            textInputStyle,
             buttonStyle,
             buttonTextStyle
     } = styles;
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text> Join a house! </Text>
-      </View>
+      <KeyboardAvoidingView behavior='padding' style={wrapperStyle} enabled>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+          <View style={containerStyle}>
+            <Text style={headerStyle}>Join a House???</Text>
+            <TextInput
+              style={textInputStyle}
+              placeholder='Enter your house ID'
+              onChangeText={
+                (houseName) => this.setState({houseID})
+              }
+              underlineColorAndroid='transparent'
+            />
+            <TouchableOpacity
+              style={buttonStyle}
+              onPress={
+                () => this.joinHouse(this.state.houseID)
+              }>
+              <Text style={buttonTextStyle}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
+
+  joinHouse = (houseID) => {
+    // console.log("IN addHouse!\n\n");
+    var user = firebase.auth().currentUser;
+    var userName = user.providerData[0].displayName;
+    var uid = user.uid;
+    var key = firebase.database().ref('/Houses').push().key;
+    firebase.database().ref('/Houses').child(key)
+      .set(
+        { HouseName: houseName,
+          Users: {uid: userName} }
+      );
+  }
+
 }
 
 const styles = StyleSheet.create({
