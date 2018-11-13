606d5e84fc4bdf7ac5209c0f5e9135df99722a23
Mary Chern <mary.chern10@gmail.com>
2018-11-12 21:13:31 -0800

    pix

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,21 +7,17 @@ import GroceryScreen from './GroceryScreen';
 import SettingsScreen from './SettingsScreen';
 
 class HomeScreen extends Component {
-  // static navigationOptions = {
-  //   headerTitle: 'Heyy',
-  // };
-
 // class LogoTitle extends React.Component {
 //   render() {
 //     return (
 //       <Image
-//         source={require('./homieIcon.png')}
+//         source={require('../assets/homieIcon.png')}
 //         style={{ width: 30, height: 30 }}
 //       />
 //     );
 //   }
 // }
-
+//
 // class HomeScreen extends Component {
 //   static navigationOptions = {
 //     headerTitle: <LogoTitle />,
