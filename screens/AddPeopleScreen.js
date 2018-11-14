6804a57fc91143dcd8bfeeb53561605f47d04682
ben-pao <paohaveyouben@gmail.com>
2018-11-13 20:52:34 -0800

    i fixed it too

--- screens/AddPeopleScreen.js
+++ screens/AddPeopleScreen.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Button, TouchableOpacity, createDrawerNavigator } from 'react-native';
-=======
 import { StyleSheet, Text, TextInput, View, TouchableOpacity,  KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
->>>>>>> master
 import * as firebase from 'firebase';
 import { Keyboard } from 'react-native';
 
@@ -59,9 +55,6 @@ class AddPeopleScreen extends Component {
     );
   }
 
-<<<<<<< HEAD
-
-=======
   emailNotNull(email) {
     if (email && email.length > 0) return true;
     else return false;
@@ -120,64 +113,11 @@ class AddPeopleScreen extends Component {
         alert(error);
       });
   }
->>>>>>> master
 
   joinHouseWithUIDHouseID(uid, userName, houseID) {
     // console.log("IN joinHouse!\n\n");
     const { navigate } = this.props.navigation;
 
-<<<<<<< HEAD
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text> You can add people to your house here! </Text>
-        <TouchableOpacity
-          style={buttonStyle}
-          onPress={
-            () => {
-              // this.joinHouse(this.state.houseID);
-              this.GetHouseID();
-            }
-          }
-        >
-          <Text style={buttonTextStyle}>get houseid</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-
-  GetHouseID (){
-  var user = firebase.auth().currentUser;
-  if(user == null){
-    return;
-  }
-  var userName = user.providerData[0].displayName;
-  var uid = user.uid;
-  //alert(uid);
-  //var key = firebase.database().ref('/Users').push().key;
-  var userDBref = firebase.database().ref('/Users').child(uid)
-  var userData = "";
-  userDBref.on('value', function(snapshot){
-    userData = snapshot.val();
-    console.log(userData.HouseID);
-    var userHouseref = firebase.database().ref('/Houses').child(userData.HouseID)
-    var housedata = "";
-    userHouseref.on('value', function(snapshot){
-      housedata = snapshot.val();
-      console.log(housedata);
-      alert(housedata.HouseName);
-        }, function (error) {
-          console.log("Error: " + error.code);
-    });
-  //  alert(housedata.HouseName);
-  } , function (error) {
-   console.log("Error: " + error.code);
-  });
-  console.log(userData);  //userdata is only local insidee that userDBref
-
-}
-
-
-=======
     var houseref = firebase.database().ref("/Houses");
     // Check if house exists
     houseref.child(houseID).once("value")
@@ -213,33 +153,6 @@ class AddPeopleScreen extends Component {
         alert(error.toString());
       });
   }
-
-
-  GetHouseID (){
-    var user = firebase.auth().currentUser;
-    if(user == null){
-      return;
-    }
-    var userName = user.providerData[0].displayName;
-    var uid = user.uid;
-    //var key = firebase.database().ref('/Users').push().key;
-    var userDBref = firebase.database().ref('/Users').child(uid)
-    var userData = "";
-
-    userDBref.once('value').then(function(snapshot){
-      userData = (snapshot.val() && snapshot.val().username);
-      console.log(userData);
-    });
-
-
-
-    // firebase.database().ref('/Users').child(uid).update(
-    //     {
-    //       houseid: ,
-    //     }
-    // );
-   }
->>>>>>> master
 }
 
 
