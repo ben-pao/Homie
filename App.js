757f429f5339769973667cd3667e7a98d9939fa0
ben-pao <paohaveyouben@gmail.com>
2018-11-15 19:40:29 -0800

    logging notification token

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import StackNavigator from './navigation/Navigation'
+import StackNavigator from './navigation/Navigation';
+import { Permissions, Notifications } from 'expo';
 
 import * as firebase from 'firebase';
 
@@ -26,7 +27,45 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  console.log("push notification token");
+  console.log(token);
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+  return;
+}
+
 export default class App extends Component {
+
+  componentDidMount() {
+    // this.registerForPushNotificationsAsync().done();
+    registerForPushNotificationsAsync();
+  }
+
+
+
+
   render() {
     return(
         <StackNavigator />
